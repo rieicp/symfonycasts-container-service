1d27b48534bd2f074e8982614d0575434bbcf558
Ning <ning.wei@int-trade.de>
2018-12-07 23:31:47 +0100

    查询 bin/console debug:container doctrine_cache.providers.my_markdown_cache      其类名 Class: Doctrine\Common\Cache\ArrayCache 但：不宜用Array Cache！
    在PHPStorm中 shift shift查找ArrayCache，不断追溯下去，
    发现会实现数个接口，其中一个叫'Cache'。Type-hint这个接口。
    
    在$cache处按Alt+Enter，然后选择 initialize fields
    可以自动生成私有类成员变量$this->cache，并赋值
    
    +++ 因为：配置了只有prod环境才缓存。勿忘：清除prod缓存，
    然后使用app.php运行prod环境，进行测试。

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -4,29 +4,35 @@
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
 class MarkdownTransformer
 {
     private $markdownParser;
+    /**
+     * @var Cache
+     */
+    private $cache;
 
-    public function __construct(MarkdownParserInterface $markdownParser)
+    public function __construct(MarkdownParserInterface $markdownParser, Cache $cache)
     {
         $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
     }
 
     public function parse($str)
     {
-        //$cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+//        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
         $key = md5($str);
-        if ($cache->contains($key)) {
-            return $cache->fetch($key);
+        if ($this->cache->contains($key)) {
+            return $this->cache->fetch($key);
         }
 
         sleep(1); // fake how slow this could be
 
         $str = $this->markdownParser->transform($str);
-        $cache->save($key, $str);
+        $this->cache->save($key, $str);
 
         return $str;
     }
